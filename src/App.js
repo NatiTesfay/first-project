ce0cc0205d7c8cc0b15c69dd01858983fb37013a
netanel tesfay <netaneltes66@gmailgit config --global user.email you@example.com.com>
2022-08-24 12:37:26 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import Header from "./coponents/pages/Header/Header"
-import Footer from "./coponents/pages/Footer/Footer"
-import Main from "./coponents/pages/Main/Main"
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>
+);
